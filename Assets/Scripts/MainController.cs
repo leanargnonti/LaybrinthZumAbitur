0d60ef7b14843a124deca9bf22feffa9e6bfed5c
Leana Rgnonti <lille_rgnonti@icloud.com>
2018-10-24 17:29:38 +0200

    Added all existing Data
    Initial Commit Of the Project

---
+++ Assets/Scripts/MainController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class MainController : MonoBehaviour {
+
+    public GameObject door1;
+    public GameObject door2;
+    public GameObject door3;
+    public GameObject door4;
+
+
+    // Use this for initialization
+    void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown("e"))
+        {
+            door1.GetComponent<GlideController>().SetDestination(new Vector3(door1.transform.position.x, door1.transform.position.y - 2f, door1.transform.position.z));
+        }
+        if (Input.GetKeyDown("r"))
+        {
+            door1.GetComponent<GlideController>().SetDestination(new Vector3(door1.transform.position.x, door1.transform.position.y + 2f, door1.transform.position.z));
+        }
+    }
+}
