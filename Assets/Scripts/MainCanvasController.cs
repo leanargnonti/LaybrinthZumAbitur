01cb5f82034bef5d7064c69f3ffd91b6fa85af49
Jesko Edzards <edzardsj>
2018-11-07 00:39:40 +0100

    Configured the UI to look properly and added tghe option to press escape to end the game

--- Assets/Scripts/MainCanvasController.cs
+++ Assets/Scripts/MainCanvasController.cs
@@ -24,7 +24,10 @@ public class MainCanvasController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
     }
 
     public void showStar()
